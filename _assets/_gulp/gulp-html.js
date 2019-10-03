6c99847cdd07841c2c0a86dce200fbd37fb75d2f
Gar <gar@lonelydatu.com>
2019-10-03 11:59:49 -0400

    lines

--- _assets/_gulp/gulp-html.js
+++ _assets/_gulp/gulp-html.js
@@ -6,8 +6,8 @@ var notify          = require("gulp-notify");
 
 function buildTemplates(project, browserSync){
 	var splitInfo = project.split('_');
-    var name = splitInfo[0].split("/")[2];
-    var size = splitInfo[1].split("x");
+    var name = splitInfo[0];
+    var size = splitInfo[splitInfo.length-1].split("x");
     var width = size[0];
     var height = size[1];
     var obj = {name: name, width:width, height:height};
